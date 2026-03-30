2741a5e32bcab7b5618d917809e48d119858f94b
Éricles Miller <ericlesmiller15@gmail.com>
2026-03-30 16:12:33 -0300

    Add IAM role configuration for ECR with OIDC provider

--- iam.tf
+++ iam.tf
@@ -11,3 +11,37 @@ resource "aws_iam_openid_connect_provider" "oidc_git" {
     IAC = "true"
   }
 }
+
+resource "aws_iam_role" "ecr_role" {
+  name = "ecr-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Principal": {
+                "Federated": "arn:aws:iam::779257801295:oidc-provider/token.actions.githubusercontent.com"
+            },
+            "Condition": {
+                "StringEquals": {
+                    "token.actions.githubusercontent.com:aud": [
+                        "sts.amazonaws.com"
+                    ]
+                },
+                "StringLike": {
+                    "token.actions.githubusercontent.com:sub": [
+                        "repo:Ericles-Miller/nestjs-ci-api:ref:refs/heads/main",
+                        "repo:Ericles-Miller/nestjs-ci-api:ref:refs/heads/main"
+                    ]
+                }
+            }
+        }
+    ]
+  })
+
+  tags = {
+    IAC = "true"
+  }
+}
