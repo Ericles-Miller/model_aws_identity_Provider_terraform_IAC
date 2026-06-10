289486896bfc443fb08ab74d6d103c9641cbfdcd
Éricles Miller <ericlesmiller15@gmail.com>
2026-06-09 22:33:44 -0300

    Add CI workflow and IAM role configurations for App Runner and ECR

--- iam.tf
+++ iam.tf
@@ -12,6 +12,29 @@ resource "aws_iam_openid_connect_provider" "oidc_git" {
   }
 }
 
+resource "aws_iam_role" "app-runner-role" {
+  name = "app-runner-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          "Service" = "build.apprunner.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+  ]
+  tags = {
+    IAC = "True"
+  }
+}
+
 resource "aws_iam_role" "ecr_role" {
   name = "ecr-role"
 
@@ -40,30 +63,47 @@ resource "aws_iam_role" "ecr_role" {
     ]
   })
 
-  inline_policy {
-    name = "ecr-app-permissions"
-    policy = jsonencode({
-      Statement = [
-        {
-          Sid = "Statement1"
-          Action = [
-            "ecr:GetDownloadUrlForLayer",
-            "ecr:BatchGetImage",
-            "ecr:BatchCheckLayerAvailability",
-            "ecr:PutImage",
-            "ecr:InitiateLayerUpload",
-            "ecr:UploadLayerPart",
-            "ecr:CompleteLayerUpload",
-            "ecr:GetAuthorizationToken",
-          ]
-          Effect   = "Allow"
-          Resource = "*"
-        }
-      ]
-    })
-  }
-
   tags = {
     IAC = "true"
   }
+}
+
+resource "aws_iam_role_policy" "ecr_policy" {
+  name = "ecr-app-permissions"
+  role = aws_iam_role.ecr_role.id
+
+  policy = jsonencode({
+    Statement = [
+      {
+        Sid      = "Statement1"
+        Action   = "apprunner:*"
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Sid = "Statement2"
+        Action = [
+          "iam:PassRole",
+          "iam:CreateServiceLinkedRole",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Sid = "Statement3"
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetAuthorizationToken",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
 }
