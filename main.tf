b830fa2a39cc74ff499b6cd497d42aceecbd065f
Éricles Miller <ericlesmiller15@gmail.com>
2026-07-06 15:59:41 -0300

    feat: Update IAM role inline policy for tf-role and add S3 bucket configuration

--- main.tf
+++ main.tf
@@ -10,3 +10,23 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "iac-nestjs"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "iac-nestjs"
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
